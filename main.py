6435e69556a2d72c3e12cc33f9a7c33fac07fa9f
Amit Hauzer <amit.hauzer@gmail.com>
2025-04-25 11:18:26 +0300

    Add current date and time print to main.py

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+from datetime import datetime
+
 def main():
-    print("Welcome to Git Assignment")
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print("Welcome to Git Assignment!!")
+    print(f"The current date and time: {current_time}")
 
 
 if __name__ == "__main__":
