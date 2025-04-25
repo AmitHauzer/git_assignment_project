a081e421cdb11ad57a794ffd6a7ee4eb37f3fe9a
Amit Hauzer <amit.hauzer@gmail.com>
2025-04-25 12:06:10 +0300

    Hotfix: add Hello world

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from datetime import datetime
 
 def main():
     current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print("Hello World")
     print("Welcome to Git Assignment!! My name is Amit.")
     print(f"The current date and time: {current_time}")
 
